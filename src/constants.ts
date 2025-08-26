0b3e17d017ebe8ffd94351946d217fe763cafbdc
OnlyCoinToken <pampersmuffin@gmail.com>
2025-08-26 16:16:10 +0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const ACTIVE_CHAIN = { id: 8453, name: "Base" };
 
 export const PRESALE_ADDRESS = "0x00ab2677723295F2d0A79cb68E1893f9707B409D";
-export const USDT_ADDRESS    = "0xfde4c96c8593536e31f229ea8f37b2ada2699bb2"; // USDC or USDT (6 decimals)
+export const USDT_ADDRESS    = "0xdac17f958d2ee523a2206206994597c13d831ec7"; // USDC or USDT (6 decimals)
 export const SAAD_ADDRESS    = "0x3120B918b65613fC709272E26938352229c2E597";
 
 // On-ramp configs (ok to leave as-is for testing)
