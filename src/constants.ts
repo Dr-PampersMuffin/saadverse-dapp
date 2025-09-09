02ae516b4ef87eb781e2668c1fe60fc9edd692d0
dr-pampersmuffin <pampersmuffin@gmail.com>
2025-09-09 19:18:41 +0300

    chore: update constants.ts with env support

--- src/constants.ts
+++ src/constants.ts
@@ -11,5 +11,20 @@ export const SAAD_ADDRESS    = "0x3120B918b65613fC709272E26938352229c2E597";
 
 // On-ramp configs (ok to leave as-is for testing)
 export const TRANSAK_ENV_URL = "https://global.transak.com";
-export const TRANSAK_API_KEY = "905b840d-5e5b-4ab8-8610-cb227636e3e6";        
-export const COINBASE_CHECKOUT_ID = "37570fdf-7968-4d67-a4d6-9ffa4c4b77dd";
+export const TRANSAK_API_KEY =
+  import.meta.env.VITE_TRANSAK_API_KEY || "905b840d-5e5b-4ab8-8610-cb227636e3e6";
+
+export const COINBASE_CHECKOUT_ID =
+  import.meta.env.VITE_COINBASE_CHECKOUT_ID || "37570fdf-7968-4d67-a4d6-9ffa4c4b77dd";
+
+
+
+
+
+
+
+
+
+
+
+
